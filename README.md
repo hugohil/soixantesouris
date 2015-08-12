5573ebaeb8b4ad13645b179be49d7d919613a10a
hugohil <hilairehugo@gmail.com>
2015-08-12 12:27:20 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Soixantesouris
+
+Soixantesouris is a completely useless tool that create an avatar for your mouse on a dedicated website.
+
+## Troubleshooting
+
+After **each** `npm i`, you'll have to [rebuild the modules](http://electron.atom.io/docs/v0.28.0/tutorial/using-native-node-modules/).
+
+## TODO
+
+- record mouse in Electron
+- send mouse position to a server through socket.io
+- make a nice website
+- add the ability to name your avatar
---
+++ main.js
@@ -0,0 +1,24 @@
+var app = require('app');  // Module to control application life.
+var BrowserWindow = require('browser-window');  // Module to create native browser window.
+
+require('crash-reporter').start();
+
+var mainWindow = null;
+
+app.on('window-all-closed', function() {
+  if (process.platform != 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('ready', function() {
+  mainWindow = new BrowserWindow({width: 600, height: 400});
+
+  mainWindow.loadUrl('file://' + __dirname + '/index.html');
+
+  mainWindow.openDevTools();
+
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  });
+});
